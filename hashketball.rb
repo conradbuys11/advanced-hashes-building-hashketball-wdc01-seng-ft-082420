e7ac18a894de6727c6e16b81033d9e840497202d
Conrad Buys <conradbuys11@gmail.com>
2020-08-12 23:20:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,13 +14,64 @@ def game_hash()
         :slam_dunks => nil
   }
   
-  home_players_array = Array.new(5)
-  away_players_array = Array.new(5)
+  #home_players_array = Array.new(5)
+  #away_players_array = Array.new(5)
   
-  home_players_array.map{|element| player_hash}
-  away_players_array.map{|element| player_hash}
+  #home_players_array.map{|element| player_hash}
+  #away_players_array.map{|element| player_hash}
   
-  home_players_array[0][:player_name] = 
+  home_players_array = [{
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+    },{
+        :player_name => nil,
+        :number => nil,
+        :shoe => nil,
+        :points => nil,
+        :rebounds => nil,
+        :assists => nil,
+        :steals => nil,
+        :blocks => nil,
+        :slam_dunks => nil
+    },{
+        :player_name => nil,
+        :number => nil,
+        :shoe => nil,
+        :points => nil,
+        :rebounds => nil,
+        :assists => nil,
+        :steals => nil,
+        :blocks => nil,
+        :slam_dunks => nil
+    },{
+        :player_name => nil,
+        :number => nil,
+        :shoe => nil,
+        :points => nil,
+        :rebounds => nil,
+        :assists => nil,
+        :steals => nil,
+        :blocks => nil,
+        :slam_dunks => nil
+    },{
+        :player_name => nil,
+        :number => nil,
+        :shoe => nil,
+        :points => nil,
+        :rebounds => nil,
+        :assists => nil,
+        :steals => nil,
+        :blocks => nil,
+        :slam_dunks => nil
+    }
+  ]
   
   hashketball = {
     :home => {
