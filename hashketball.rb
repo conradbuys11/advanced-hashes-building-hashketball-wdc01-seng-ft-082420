9ed56c1d9d11e36770c4f603973a7f3ac38a4182
Conrad Buys <conradbuys11@gmail.com>
2020-08-12 22:59:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,24 @@
 # Write your code here!
+
+def game_hash()
+  hashketball = {
+    :home => {
+      :team_name => nil,
+      :colors => [],
+      :players => [
+        {
+          :player_name => nil,
+          :number => nil,
+          :shoe => nil,
+          :points => nil,
+          :rebounds => nil,
+          :assists => nil,
+          :steals => nil,
+          :blocks => nil,
+          :slam_dunks => nil
+        }
+        ]
+    },
+  }
+  
+end
